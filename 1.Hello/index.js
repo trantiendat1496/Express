eea8481dea6765fb03c9f0e31ebb8844e47d3321
Tien Dat <datttbhaf180235@fpt.edu.vn>
2020-05-31 15:57:57 +0700

    first commit

---
+++ 1.Hello/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = 999;
+
+// app.get('/', function(req, res){
+// 	res.send('<a href="/todos">This is a link</a>')
+// });
+
+app.get('/toDo', function(req, res){
+	res.send('<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học Code</li></ul>');
+});
+
+app.listen(port, function(){
+	console.log('Server listenning on port ' + port);
+});
