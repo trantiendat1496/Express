003637eba0f5c30585a049154234168dae2231dd
Tien Dat <datttbhaf180235@fpt.edu.vn>
2020-05-31 16:07:33 +0700

    Add link

--- 1.Hello/index.js
+++ 1.Hello/index.js
@@ -3,9 +3,9 @@ var app = express();
 
 var port = 999;
 
-// app.get('/', function(req, res){
-// 	res.send('<a href="/todos">This is a link</a>')
-// });
+app.get('/', function(req, res){
+	res.send('<a href="/todos">This is a link</a>')
+});
 
 app.get('/toDo', function(req, res){
 	res.send('<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học Code</li></ul>');
