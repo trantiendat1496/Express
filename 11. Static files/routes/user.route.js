c51135cab9078c3b884b8ed82d4bcb4ffa034bd3
trantiendat1496 <51437420+trantiendat1496@users.noreply.github.com>
2020-07-28 10:53:53 +0700

    first commit

---
+++ 11. Static files/routes/user.route.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var controller = require('../controllers/user.controller');
+
+var router = express.Router();
+
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/view/:id', controller.edit);
+
+router.get('/:id', controller.delete);
+
+router.post("/create", controller.postCreate);
+
+router.post("/update/:id", controller.postUpdate);
+
+module.exports = router;
---
+++ 12. Server-side validation (Login)/routes/todo.route.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var controller = require('../controllers/todo.controller');
+
+var router = express.Router();
+
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create',controller.create);
+
+router.get('/:id', controller.get);
+
+router.post("/create", controller.postCreate);
+
+
+module.exports = router;
